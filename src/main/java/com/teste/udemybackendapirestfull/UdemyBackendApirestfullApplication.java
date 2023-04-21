72bce05b361ef8dcb1dea45f355052074e033687
Elton Standerski <eltonstan@yahoo.com.br>
2023-04-21 19:13:54 -0300

    Primeiro commit projeto curso

---
+++ src/main/java/com/teste/udemybackendapirestfull/UdemyBackendApirestfullApplication.java
@@ -0,0 +1,13 @@
+package com.teste.udemybackendapirestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyBackendApirestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyBackendApirestfullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/udemybackendapirestfull/UdemyBackendApirestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.udemybackendapirestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyBackendApirestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
